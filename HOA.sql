944dfbb646a6352346f6b9a6eb014c6e1a26d065
Patrick Henry <108245467+Poodfoison@users.noreply.github.com>
2022-08-05 09:45:07 +0800

    Add files via upload

--- HOA.sql
+++ HOA.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE "HOA"
 
 
-    CREATE TABLE IF NOT EXISTS public."HomeOwner"
+CREATE TABLE IF NOT EXISTS public."HomeOwner"
 (
     "AccountID" integer NOT NULL,
     "First Name" "char",
     "Last Name" "char",
     "Contact" integer,
     "Email" "char",
-    "Address" "char",
     CONSTRAINT "HomeOwner_pkey" PRIMARY KEY ("AccountID")
 )
 
@@ -17,15 +16,21 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public."HomeOwner"
     OWNER to postgres;
 
-    CREATE TABLE IF NOT EXISTS public."Billing"
+
+
+    CREATE TABLE IF NOT EXISTS public."Transactions"
 (
     "TransactionID" integer NOT NULL,
     "AccountID" integer NOT NULL,
-    "Water Bill" integer,
-    "Monthly Dues" integer,
+    "BillID" integer NOT NULL,
     CONSTRAINT "Billing_pkey" PRIMARY KEY ("TransactionID"),
     CONSTRAINT "AccountID" FOREIGN KEY ("AccountID")
         REFERENCES public."HomeOwner" ("AccountID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT "BillID" FOREIGN KEY ("BillID")
+        REFERENCES public."Bill" ("BillID") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -33,8 +38,9 @@ ALTER TABLE IF EXISTS public."HomeOwner"
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."Billing"
+ALTER TABLE IF EXISTS public."Transactions"
     OWNER to postgres;
+
 
     CREATE TABLE IF NOT EXISTS public."Visitors"
 (
@@ -55,4 +61,43 @@ ALTER TABLE IF EXISTS public."Billing"
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."Visitors"
+    OWNER to postgres;
+
+
+    CREATE TABLE IF NOT EXISTS public."Bill"
+(
+    "BillID" integer NOT NULL,
+    "Name" "char",
+    "Amount" integer,
+    CONSTRAINT "Bill_pkey" PRIMARY KEY ("BillID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Bill"
+    OWNER to postgres;
+
+
+
+    CREATE TABLE IF NOT EXISTS public."Address"
+(
+    "AddressID" integer NOT NULL,
+    "Block" integer,
+    "Lot" integer,
+    "Street" "char",
+    "Barangay" "char",
+    "City" "char",
+    "Province" "char",
+    "AccountID" integer NOT NULL,
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("AddressID"),
+    CONSTRAINT "AccountID" FOREIGN KEY ("AccountID")
+        REFERENCES public."HomeOwner" ("AccountID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Address"
     OWNER to postgres;
