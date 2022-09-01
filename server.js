2a62156753cce71d05127377bf45bcc39398b31b
Patrick Henry <108245467+Poodfoison@users.noreply.github.com>
2022-09-01 13:08:55 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 const app = express();
 import { uid } from 'uid';
+import Pool from "pg";
+import  bcrypt  from  "bcryptjs"
 
+
+const pool = new Pool.Pool({ 
+    user:  'postgres',
+    password:  '0800',
+	database:  'HOA',
+    host: 'localhost'
+})
+
+
+    
 
 const logger = (req, res, next) => {
-    console.log(`request url: ${req.url} and request method: ${req.method}`)
+    console.log(`request url: ${req.url} and request method: ${req.method} ${req.connection.remoteAddress}`)
     next()
 }
 
@@ -13,28 +25,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-let users = [
-    {
-        id: "078b0e18dc77a8f597475916f645805f",
-        username: "poodfoison",
-        password: "123456789"
-    }
-]
-
-
+//let users = [
+//    {
+//        id: "078b0e18dc77a8f597475916f645805f",
+//        username: "poodfoison",
+//        password: "123456789"
+//    }
+//]
+// 
 app.get('/', (req, res) => {
     res.send("Home");
 });
 
-app.get('/users/:username', (req, res) => {
-    console.log(req.params)
-    res.json(users[req.params.username])
+
+
+app.get('/users', async(req,res) =>{
+    try {
+        const data = await pool.query(`
+        SELECT * FROM login
+        `)
+        res.json(data.rows)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/data', logger, (req, res) => {
+app.get('/users/:username', async (req, res) => {
 
-    res.json(users)
+    try {
+        const username = req.params.username
+
+        const users = await pool.query(`
+        SELECT * FROM login where username = $1
+        `, [username])
+        res.json(users.rows)
+    } catch (error) {
+        console.log(error)
+    }
 })
+
 
 app.get('/secret', (req, res) => {
     res.status(403).end();
@@ -44,62 +73,134 @@ app.get('*', (req, res) => {
     res.status(404).send('Error 404 Page not found')
 })
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body
-    console.log(username, password)
+app.post('/login', async (req, res) => {
+    try {
 
 
+        const { username, password } = req.body
 
-    users.forEach((user) => {
-        if (user.username === username) {
-            if (user.password === password) {
-                res.send("Login Successful")
-            }
-            else {
-                res.send("Incorrect Password")
-            }
+        const users = await pool.query(`SELECT * FROM login WHERE
+        username = $1`, [username])
+
+        if (users.rows.length < 0) {
+            res.status(401).send("User does not exists")
+        }
+
+        const validPassword = await bcrypt.compare(password, users.rows[0].password)
+        if (!validPassword) {
+            return res.status(401).json("Password incorrect")
+        }
+
+        res.json(users.rows)
+
+    } catch (error) {
+        console.error(error.message);
+
+        }
+
+    });
+
+app.post('/changepassword', async(req, res) => {
+
+    try {
+
+        const { username, newPassword } = req.body
+
+        const users = await pool.query(`SELECT * FROM login WHERE
+        username = $1`, [username])
+
+        if (users.rows.length < 0) {
+            res.status(401).send("User does not exists")
         }
         else {
-            res.send("Invalid User")
-        }
-    })
-});
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
 
-app.post('/changepassword', (req, res) => {
-    const { username, newPassword } = req.body
+        const bcryptPassword = await bcrypt.hash(newPassword, salt);
 
-    users.forEach((user) => {
-        if (user.username === username) {
-            user['password'] = newPassword
-            res.send(user)
-        }
-        else {
-            res.send("Invalid User")
-        }
-    })
-
-})
-
-app.post('/register', (req, res) => {
-    const { username, password } = req.body
-
-
-    users.forEach((user) => {
-        if (user.username === username) {
-               res.send("Username already in use")
-        }
-        else {
-            users.push({
-                id: uid(),
-                username,
-                password
-            })
-            res.status(201).send(`Account Successfully Created`)
-        }
-    })
    
+        const newPW = await pool.query(`
+        UPDATE login 
+        SET password = $2
+        WHERE username = $1
+        RETURNING *
+        `, [username, bcryptPassword])
+
+        res.json(newPW.rows)
+        }
+ 
+
+    } catch (error) {
+        console.error(error.message);
+
+        }
+
 })
 
-app.listen(8000, () => {
-    console.log("server started http://localhost:8000");
+
+app.post('/register', async (req, res) => {
+    try {
+
+        const {username,password} = req.body
+
+        const user = await pool.query(`SELECT * FROM login WHERE
+        username = $1`, [username])
+
+        if (user.rows.length > 0) {
+            res.status(401).send("Username already in use")
+        }
+        else  { 
+
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptPassword = await bcrypt.hash(password, salt);
+
+   
+        const newUser = await pool.query(`
+        INSERT INTO login (id, username, password)
+        VALUES ($1, $2, $3) RETURNING *
+        `, [uid(), username, bcryptPassword])
+
+        res.json(newUser.rows)
+    }
+    } catch (error) {
+
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+
+})
+
+//app.post('/register', (req, res) => {
+//    const { username, password } = req.body
+//
+//
+//    users.forEach((user) => {
+//        if (user.username === username) {
+//               res.send("Username already in use")
+//        }
+//        else {
+//            users.push({
+//                id: uid(),
+//                username,
+//                password
+//            })
+//            res.status(201).send(`Account Successfully Created`)
+//        }
+//    })
+//   
+//})
+
+
+
+pool.connect((err)=>{
+    if (err){
+        console.log(err.message)
+    }
+    else{
+        app.listen(8000, () => {
+            console.log("server started http://localhost:8000");
+        })
+    }
 })
