a144631c81837daf7926c583648ddb37d60019d4
Patrick Henry <108245467+Poodfoison@users.noreply.github.com>
2022-09-26 15:13:06 +0800

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -54,11 +54,7 @@ app.get('/users/:username', async (req, res) => {
 })
 
 app.get('/secret', logger, (req, res) => {
-    res.status(403).end();
-})
-
-app.get('*', logger, (req, res) => {
-    res.status(404).send('Error 404 Page not found')
+res.status(403).end();
 })
 
 app.post('/login', async (req, res) => {
@@ -70,13 +66,14 @@ app.post('/login', async (req, res) => {
         const user = await pool.query(`SELECT * FROM login WHERE
         username = $1`, [username])
 
-        if (user.rows[0].length < 0) {
-            res.status(401).send("User does not exists")
+        if (user.rows.length <= 0) {
+            res.status(401).send({msg:"User does not exists"})
         }
 
         const validPassword = await bcrypt.compare(password, user.rows[0].password)
         if (!validPassword) {
-            return res.status(401).json("Password incorrect")
+            return res.status(401).json({msg:"Password incorrect"})
+            
         }
 
         const token = generateJWT(user.rows[0])
@@ -88,19 +85,13 @@ app.post('/login', async (req, res) => {
 
     });
 
-app.post('/changepassword', async(req, res) => {
+app.post('/changepassword', auth, async(req, res) => {
 
     try {
 
-        const { username, newPassword } = req.body
+        const {newPassword} = req.body
 
-        const user = await pool.query(`SELECT * FROM login WHERE
-        username = $1`, [username])
-
-        if (user.rows.length < 0) {
-            res.status(401).send("User does not exists")
-        }
-        else {
+        
         const saltRound = 10;
         const salt = await bcrypt.genSalt(saltRound);
 
@@ -110,14 +101,14 @@ app.post('/changepassword', async(req, res) => {
         const updatePassword = await pool.query(`
         UPDATE login 
         SET password = $2
-        WHERE username = $1
+        WHERE id = $1
         RETURNING *
-        `, [username, bcryptPassword])
+        `, [req.users.id, bcryptPassword])
 
         res.json(updatePassword.rows)
-        }
+        
  
-
+        
     } catch (error) {
         console.log(error.message)
 
@@ -126,46 +117,119 @@ app.post('/changepassword', async(req, res) => {
 
 })
 
-
-app.post('/register', async (req, res) => {
+app.post('/addupdates', async (req, res) => {
     try {
+        const {updates} = req.body
 
-        const {username,password} = req.body
+        const d = new Date();
+        let newBillUID = uid()
 
-        const user = await pool.query(`SELECT * FROM login WHERE
-        username = $1`, [username])
+        // current date
+        let date = ("0" + d.getDate()).slice(-2);
 
-        if (user.rows.length > 0) {
-            res.status(401).send("Username already in use")
-        }
-        else  { 
+        // current month
+        let month = ("0" + (d.getMonth() + 1)).slice(-2);
 
-        const saltRound = 10;
-        const salt = await bcrypt.genSalt(saltRound);
+        // current year
+        let year = d.getFullYear();
+        
+        let day = year + "-" + date + "-" + month;
 
-        const bcryptPassword = await bcrypt.hash(password, salt);
-
-   
-        const newUser = await pool.query(`
-        INSERT INTO login (id, username, password)
+        const newupdates = await pool.query(`
+        INSERT INTO updates (updateid, date, updates)
         VALUES ($1, $2, $3) RETURNING *
-        `, [uid(), username, bcryptPassword])
- 
-        const token = generateJWT(newUser.rows[0])
+        `, [newBillUID, day, updates])
+        
+        res.json(newupdates.rows)
 
-    res.json({ token })
-    }
+
+
     } catch (error) {
-
         console.log(error.message)
         res.status(500).send(error.message)
-    }
 
+    }
 })
+
+app.get('/updates', async(req,res) =>{
+    try {
+        const data = await pool.query(`
+        SELECT * FROM updates
+        `)
+        res.json(data.rows)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+app.get('/account', auth, async(req,res) =>{
+    try {
+        
+        const account = await pool.query(`
+        SELECT firstname, lastname, contact, email, block, lot, street, barangay, city, province  FROM login
+        INNER JOIN address
+        ON address.id = login.id
+        WHERE login.id = $1
+        `,[req.users.id])
+        res.json(account.rows)
+
+    } catch (error) {
+        console.log(error.message)
+
+
+        }
+
+})     
+
+app.get('/address', auth, async(req,res) =>{
+    try {
+        
+        const account = await pool.query(`
+        SELECT block, lot, street, barangay, city, province  FROM address
+        INNER JOIN login
+        ON login.id = address.id
+        WHERE login.id = $1
+        `,[req.users.id])
+        res.json(account.rows)
+
+    } catch (error) {
+        console.log(error.message)
+
+
+        }
+
+})     
 
 app.get('/profile', auth, async (req, res) => {
     try {
-        res.json(req.user)
+        res.json(req.users)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ msg: "Unauthenticated" });
+
+    }
+})
+
+app.get('/bill', auth, async (req, res) => {
+    try {
+
+
+        const account = await pool.query(`
+        SELECT transactionid, name, contact, email, block, lot, street, barangay, city, province, date, amount  FROM login
+        INNER JOIN address
+        ON address.id = login.id
+        INNER JOIN transaction
+        ON transaction.id = login.id
+        INNER JOIN bill
+        ON transaction.billid = bill.billid
+        WHERE login.id = $1
+        `,[req.users.id])
+        res.json(account.rows)
+
+
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send({ msg: "Unauthenticated" });
@@ -174,14 +238,187 @@ app.get('/profile', auth, async (req, res) => {
 })
 
 
-app.get('/profile2', async(req,res) =>{
-    try{
-        res.json("Hello World")
-    }
-    catch(error){
+app.post('/addbill', auth, async (req, res) => {
+    try {
+        const {name,amount} = req.body
+
+        const d = new Date();
+        let newBillUID = uid()
+
+        // current date
+        let date = ("0" + d.getDate()).slice(-2);
+
+        // current month
+        let month = ("0" + (d.getMonth() + 1)).slice(-2);
+
+        // current year
+        let year = d.getFullYear();
+        
+        let day = year + "-" + date + "-" + month;
+
+        const newbill = await pool.query(`
+        INSERT INTO bill (billid, date, name, amount)
+        VALUES ($1, $2, $3, $4) RETURNING *
+        `, [newBillUID, day, name, amount])
+
+        const newTransaction = await pool.query(`
+        INSERT INTO transaction (transactionid, id, billid)
+        VALUES ($1, $2, $3) RETURNING *
+        `, [uid(), req.users.id, newBillUID])
+        
+        res.json(newbill.rows)
+
+
+
+    } catch (error) {
         console.log(error.message)
+        res.status(500).send(error.message)
+
     }
-} )
+})
+
+
+
+
+
+
+app.post('/updateaddress', auth, async(req, res) => {
+
+    try {
+        const {block, lot, street, barangay, city, province} = req.body
+
+         
+
+        const updateAddress = await pool.query(`
+        UPDATE address
+        SET (block, lot, street, barangay, city, province) = ($2, $3, $4, $5, $6, $7)
+        WHERE id = $1
+        RETURNING *
+        `, [req.users.id, block, lot, street, barangay, city, province ])
+        
+        res.json(updateAddress.rows)
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+
+    }
+})
+
+app.post('/updatelogin', auth, async(req, res) => {
+
+    try {
+        const {firstname, lastname, contact, email} = req.body
+
+         
+
+        const updateLogin = await pool.query(`
+        UPDATE login
+        Set (firstname, lastname, email, contact) = ($2, $3, $4, $5)
+        WHERE id = $1
+        RETURNING *
+        `, [req.users.id, firstname, lastname, email, contact ])
+        
+        res.json(updateLogin.rows)
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+
+    }
+})
+
+app.post('/addvisitor', auth, async (req, res) => {
+    try {
+        const {firstname, lastname, contact, validid} = req.body
+
+         
+
+        const visitors = await pool.query(`
+        INSERT INTO visitor (visitorid, id, firstname, lastname, contact, validid)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *
+        `, [uid(), req.users.id, firstname, lastname, contact, validid ])
+        
+        res.json(visitors.rows)
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+
+    }
+})
+
+app.post('/message', auth, async (req, res) => {
+    try {
+        const {name, email, contact, message} = req.body
+
+         
+
+        const messageContact = await pool.query(`
+        INSERT INTO contact (concernid, name, email, contact, message)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *
+        `, [uid(), name, email, contact, message])
+        
+        res.json(messageContact.rows)
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+
+    }
+})
+
+app.post('/register', async (req, res) => {
+    try {
+
+        const {username,password,firstname,lastname,contact,email,block, lot, street, barangay, city, province} = req.body
+
+        const user = await pool.query(`SELECT * FROM login WHERE
+        username = $1`, [username])
+
+        if (user.rows.length > 0) {
+            res.status(401).send({msg:"Username already in use"})
+        }
+        else  { 
+
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptPassword = await bcrypt.hash(password, salt);
+        
+        let newUID = uid()
+   
+        const newUser = await pool.query(`
+        INSERT INTO login (id, username, password, firstname, lastname, contact, email)
+        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *
+        `, [newUID, username, bcryptPassword, firstname, lastname, contact, email])
+
+        const newAddress = await pool.query(`
+        INSERT INTO address (addressid, block, lot, street, barangay, city, province,id)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *
+        `, [uid(), block, lot, street, barangay, city, province, newUID])
+        
+   
+ 
+        const token = generateJWT(newUser.rows[0],newAddress.rows[0])
+
+    res.json({ token })
+    }
+} catch (error) {
+
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+
+})
+
+
+
+
 
 
 pool.connect((err)=>{
